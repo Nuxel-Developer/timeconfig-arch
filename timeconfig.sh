2c0efec4a9b63c2f7e02970215358c3b686f6c2e
Rafael <105615289+Nuxel-Developer@users.noreply.github.com>
2022-06-25 15:28:10 -0300

    update of timeconfig.sh

--- timeconfig.sh
+++ timeconfig.sh
@@ -1,28 +1,37 @@
 #!/bin/env bash
 TIME_ZONE=$(curl -f -s -S "https://ipapi.co/timezone")
+OS_NAME=""
 
 function debug_error {
     echo "Error: $*"
 }
 
-if [ "$1" == "--spt" ]; then
+source <(cat /etc/os-release | grep PRETTY_NAME)
+
+OS_NAME=${PRETTY_NAME}
+
+if [ "${OS_NAME}" == "Arch Linux" ]; then
+  if [ "$1" == "--spt" ]; then
     echo "Starting configuration..."
     printf "Do you want to set timezone manually or automatilly? (a/m) "
     read junk
     if [ ${junk} == "a" ]; then
         exec timedatectl set-timezone ${TIME_ZONE}
-        echo "Done, Timezone changed to ${TIME_ZONE}"
+        echo -e "Done, Timezone changed to ${TIME_ZONE}"
     elif [ ${junk} == "m" ]; then
         printf "Set timezone (e.g.: America/Chicago): "
         read ans
         if [ ${ans} != "" ]; then
             exec timedatectl set-timezone ${ans}
-            echo "Done, Timezone changed to ${ans}"
+            echo -e "Done, Timezone changed to ${ans}"
         fi
     else
         echo "No valid answear, exiting"
         exit 0
     fi
-else
+ else
     debug_error "No command found"
+ fi
+else
+    echo "The OS $OS_NAME is not compatible with timeconfig"
 fi
